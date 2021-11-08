9faa8d8cd007110c4da4ec90c54084014622a951
Revella-Jhansi <92453216+Revella-Jhansi@users.noreply.github.com>
2021-11-08 07:23:09 +0530

    Add files via upload

---
+++ DuplicateEmployeeIdException.java
@@ -0,0 +1,8 @@
+package com.sonata.assignment4;
+
+class DuplicateEmployeeIdException extends Exception{
+    public DuplicateEmployeeIdException(String message)
+    {
+        super(message);
+    }
+}
