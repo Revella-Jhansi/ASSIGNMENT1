a2cbbfd6d39db8ce2fcd37ddbabec7aa701ace85
Revella-Jhansi <92453216+Revella-Jhansi@users.noreply.github.com>
2021-11-08 07:18:57 +0530

    Add files via upload

---
+++ Shape.java
@@ -0,0 +1,21 @@
+package com.sonta.day_3;
+
+
+public class Shape {
+	float Length;
+	float Width;
+	float Height;
+	
+	public Shape() { }
+		
+		public Shape(float length, float width, float height) {
+			this.Length=length;
+			this.Width=width;
+			this.Height=height;
+			
+		}
+
+		public float area(float length, float width, float height) {
+			return 0;
+		}
+	}
