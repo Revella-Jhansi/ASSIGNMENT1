4337dd8846a5d6c68d45d693792fd37707f82531
Revella-Jhansi <92453216+Revella-Jhansi@users.noreply.github.com>
2021-11-08 07:21:02 +0530

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,16 @@
+package com.sonata.assignments3;
+
+
+public abstract class Employee {
+
+		long employeeid;
+		String employeeName;
+		Address address;
+		double basicpay;
+		
+		 abstract double calculatesalary();
+
+	}
+
+
+
