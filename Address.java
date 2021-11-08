c017f00899db96cc262d302da6b0712db2212ddf
Revella-Jhansi <92453216+Revella-Jhansi@users.noreply.github.com>
2021-11-08 07:20:43 +0530

    Add files via upload

---
+++ Address.java
@@ -0,0 +1,21 @@
+package com.sonata.assignments3;
+
+public class Address {
+	String doorno;
+	String Street;
+	String city;
+	int pincode;
+	public Address(String doorno, String street, String city, int pincode) {
+		super();
+		this.doorno = doorno;
+		Street = street;
+		this.city = city;
+		this.pincode = pincode;
+	}
+	public Address(String string) {
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
+
